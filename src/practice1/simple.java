995a016d7ea143cb6809bb81ff6db1ad7f7f7fb2
mahadev sakhare <sakhare4779@gmail.com>
2022-08-16 23:07:57 -0700

    practice

---
+++ src/practice1/simple.java
@@ -0,0 +1,16 @@
+package practice1;
+
+public class simple {
+    public static void main(String[] args) {
+       // int a=10;
+       // float f=a;
+       // float b=10.5f;
+       // int c=(int)b;
+        int a=130;
+        short b=(short)a;// data type overflow
+        System.out.println("value a ="+a);
+        System.out.println("value a ="+b);
+// this program is type casting change  in one data type to another data type
+    }
+
+}
